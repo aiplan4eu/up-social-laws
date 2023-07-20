ec28aed940976d07cb7477e166ed4dd2c3aa64d0
Erez Karpas <karpase@gmail.com>
2023-07-20 11:51:40 +0300

    go back to simple setup.py

--- setup.py
+++ setup.py
@@ -13,26 +13,6 @@ long_description=\
     up_social_laws is a package that allows for various compilations and checks related to social law verification and synthesis.
 '''
 
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-
-    class bdist_wheel(_bdist_wheel):
-
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            # Mark us as not a pure python package
-            self.root_is_pure = False
-
-        def get_tag(self):
-            python, abi, plat = _bdist_wheel.get_tag(self)
-            # We don't link with python ABI, but require python3
-            python, abi = 'py3', 'none'
-            return python, abi, plat
-except ImportError:
-    bdist_wheel = None
-
-
-
 setup(name='up_social_laws',
       version=up_social_laws.__version__,
       description='Unified Planning Integration of Social Laws',
@@ -45,9 +25,6 @@ setup(name='up_social_laws',
                'Topic :: Scientific/Engineering :: Artificial Intelligence'
                ],
       packages=['up_social_laws'],
-    #   cmdclass={
-    #       'bdist_wheel': bdist_wheel
-    #   },
       install_requires=[],
       python_requires='>=3.7',
       license='APACHE'
