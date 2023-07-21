86948373cb35dbcebb8a83c48f7a08a0a7fdb713
Erez Karpas <karpase@gmail.com>
2023-07-21 07:40:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ long_description=\
 setup(name='up_social_laws',
       version=up_social_laws.__version__,
       description='Unified Planning Integration of Social Laws',
+      long_description='Integration of Social Laws into the Unified Planning Framework',      
       author='Technion Cognitive Robotics Lab',
       author_email='karpase@technion.ac.il',
       url='https://github.com/aiplan4eu/up-social-laws',
